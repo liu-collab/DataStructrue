daec08e13739964fb0a9bff3741de09892622799
lay <1581133593@qq.com>
2021-10-13 15:09:56 +0800

    树

--- 树/01-二叉搜索树.js
+++ 树/01-二叉搜索树.js
@@ -44,7 +44,49 @@ function BinarySearchTree() {
     };
   };
   // 2- search(key):查找一个 key,存在返回 true
+  BinarySearchTree.prototype.search = function (key) {
+    //循环判断
+    let node = this.root;
+    while (node != null) {
+      if (key < node.key) {
+        node = node.left;
+      } else if (key > node.key) {
+        node = node.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+
+    //递归调用
+    //return this.searchNode(this.root, key);
+  };
+  BinarySearchTree.prototype.searchNode = function (node, key) {
+    if (node == null) return false;
+    if (node.key > key) {
+      //大于往左边找
+      return this.searchNode(node.left, key);
+    } else if (node.key < key) {
+      //小于往右边找
+      return this.searchNode(node.right, key);
+    } else {
+      return true; //等于返回为true
+    }
+  };
   // 3- inOrderTraverse:中序遍历
+  BinarySearchTree.prototype.inOrderTraverse = function (handle) {
+    this.inOrderTraverseNode(this.root, handle);
+  };
+  BinarySearchTree.prototype.inOrderTraverseNode = function (node, handle) {
+    if (node != null) {
+      //1.先访问左节点
+      this.inOrderTraverseNode(node.left, handle);
+      //2.处理节点
+      handle(node.key);
+      //3.访问右节点
+      this.inOrderTraverseNode(node.right, handle);
+    }
+  };
   // 4- preOrderTraverse:先序遍历
   BinarySearchTree.prototype.preOrderTraversal = function (handle) {
     //查找子节点
@@ -65,8 +107,36 @@ function BinarySearchTree() {
     }
   };
   // 5- postOrderTraverse:后序遍历
+  BinarySearchTree.prototype.postOrderTraverse = function (handle) {
+    this.postOrderTraverseNode(this.root, handle);
+  };
+  BinarySearchTree.prototype.postOrderTraverseNode = function (node, handle) {
+    if (node != null) {
+      //访问左节点
+      this.postOrderTraverseNode(node.left, handle);
+      //访问右节点
+      this.postOrderTraverseNode(node.right, handle);
+      //处理节点
+      handle(node.key);
+    }
+  };
   // 6- min:返回最小值
+  BinarySearchTree.prototype.min = function () {
+    let node = this.root;
+
+    while (node.left != null) {
+      node = node.left;
+    }
+    return node.key;
+  };
   // 7- max:返回最大值
+  BinarySearchTree.prototype.max = function () {
+    let node = this.root;
+    while (node.right != null) {
+      node = node.right;
+    }
+    return node.key;
+  };
   // 8- remove(key):删除 key
 }
 
@@ -96,3 +166,14 @@ const handle = function (key) {
 };
 bst.preOrderTraversal(handle);
 console.log(`先序遍历:${resString}`);
+resString = '';
+bst.inOrderTraverse(handle);
+console.log(`中序遍历:${resString}`);
+resString = '';
+bst.postOrderTraverse(handle);
+console.log(`后序遍历:${resString}`);
+
+console.log(bst.min());
+console.log(bst.max());
+console.log(bst.search(25));
+console.log(bst.search(95));
