96ba0fe09ea072481040150f695a48f3ed9d6837
lay <1581133593@qq.com>
2021-10-24 08:57:26 +0800

    promise

--- 00-力扣/01-简单题/03-promise设计.js
+++ 00-力扣/01-简单题/03-promise设计.js
@@ -3,36 +3,56 @@ class Mypormise {
     this.status = 'pendding';
     this.resolved = undefined;
     this.rejected = undefined;
+    this.onResolvedFns = [];
+    this.onRejectedFns = [];
     const resolve = (val) => {
-      if (this.status == 'pedding') {
-        //在进来的时候改变状态,不能放在在微任务队列中改状态
-        this.status = 'resolved';
+      if (this.status == 'pendding') {
+        //在队列中先判断当前的状态,不满足对应的状态直接return,防止resolve和reject一起调用
         queueMicrotask(() => {
+          if (this.status !== 'pendding') return;
+          this.status = 'resolved';
           this.resolved = val;
-          this.onResolved(this.resolved);
+          //遍历数组中的函数
+          this.onResolvedFns.forEach((fn) => {
+            fn(this.resolved);
+          });
         });
       }
     };
     const reject = (rejected) => {
       if (this.status == 'pendding') {
-        this.status = 'rejected';
         queueMicrotask(() => {
+          if (this.status !== 'pendding') return;
+          this.status = 'rejected';
           this.rejected = rejected;
-          this.onRejected(this.rejected);
+          this.onRejectedFns.forEach((fn) => {
+            fn(this.rejected);
+          });
         });
       }
     };
     excutor(resolve, reject);
   }
   then(onResolved, onRejected) {
-    this.onResolved = onResolved;
-    this.onRejected = onRejected;
+    //判断当前的状态为resolved直接执行
+    //在定时器中会直接改变状态,如果不直接执行,状态不是pendding
+    if (this.status == 'resolved') {
+      onResolved(this.resolved);
+    }
+    if (this.status == 'rejected') {
+      onRejected(this.rejected);
+    }
+    //将多个then执行放入到数组中,遍历执行数组中的函数
+    if (this.status == 'pendding') {
+      this.onResolvedFns.push(onResolved);
+      this.onRejectedFns.push(onRejected);
+    }
   }
 }
 
 const pormise = new Mypormise((resolve, reject) => {
   resolve(111);
-  reject(222);
+  //reject(222);
 });
 
 pormise.then(
@@ -43,3 +63,23 @@ pormise.then(
     console.log(err);
   }
 );
+
+pormise.then(
+  (res) => {
+    console.log(res);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+setTimeout(() => {
+  pormise.then(
+    (res) => {
+      console.log(res);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+}, 1000);
