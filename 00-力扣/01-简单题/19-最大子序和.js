cee27432c8efff705ea7f8ba9c317726705ea879
lay <1581133593@qq.com>
2021-11-04 21:09:44 +0800

    力扣

--- 00-力扣/01-简单题/19-最大子序和.js
+++ 00-力扣/01-简单题/19-最大子序和.js
@@ -3,13 +3,14 @@ var maxSubArray = function (nums) {
   dp[0] = nums[0];
   //当前的数和后面的数进行累加操作
   for (let i = 0; i < nums.length; i++) {
-    if (dp[i - 1] > 0) {
-      //判断累加的和是否大于零,大于零表示可以往前累加
-      //否则直接赋值即可
-      dp[i] = dp[i - 1] + nums[i];
-    } else {
-      dp[i] = nums[i];
-    }
+    // if (dp[i - 1] > 0) {
+    //   //判断累加的和是否大于零,大于零表示可以往前累加
+    //   //否则直接赋值即可
+    //   dp[i] = dp[i - 1] + nums[i];
+    // } else {
+    //   dp[i] = nums[i];
+    // }
+    dp[i] = dp[i - 1] > 0 ? dp[i - 1] + nums[i] : nums[i];
   }
 
   return Math.max(...dp);
