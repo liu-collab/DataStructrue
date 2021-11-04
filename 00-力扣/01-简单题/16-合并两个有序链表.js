fb25340d5cdb1e4fde1649515a4fb731d39bb1b2
lay <1581133593@qq.com>
2021-11-04 19:35:28 +0800

    力扣

---
+++ 00-力扣/01-简单题/16-合并两个有序链表.js
@@ -0,0 +1,17 @@
+var mergeTwoLists = function (l1, l2) {
+  if (l1 == null) {
+    return l2;
+  }
+  if (l2 == null) {
+    return l1;
+  }
+  //比较两个节点的大小
+  //总是返回两个链表中节点小的数据
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
+};
