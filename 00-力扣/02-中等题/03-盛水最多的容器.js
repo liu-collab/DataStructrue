3eb9f9504b2d97882b006f2a808bd36a5a415dd7
liu-collab <1581133593@qq.com>
2021-11-16 09:33:41 +0800

    11/16

---
+++ 00-力扣/02-中等题/03-盛水最多的容器.js
@@ -0,0 +1,16 @@
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let res = 0;
+  while (left < right) {
+    min = Math.min(height[left], height[right]);
+    let area = min * (right - left);
+    res = Math.max(area, res);
+    if (height[left] <= height[right]) {
+      ++left;
+    } else {
+      --right;
+    }
+  }
+  return res;
+};
