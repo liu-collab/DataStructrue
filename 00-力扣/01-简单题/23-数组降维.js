3cc995afdf9d00b16fff44ab08e3ab7411b5091c
liu-collab <1581133593@qq.com>
2021-11-17 18:28:17 +0800

    11/17

--- 00-力扣/01-简单题/23-数组降维.js
+++ 00-力扣/01-简单题/23-数组降维.js
@@ -1,21 +1,29 @@
 let arr = [1, [12, 3], [1, [1, 2, 3], 3]];
 //方法一:字符串拼接
-// arr += " ";
-// arr = arr.split(",");
-// console.log(arr);
+function bar(arr) {
+  const res = [];
+  arr += " ";
+  arr = arr.split(",");
+  for (let item of arr) {
+    res.push(parseInt(item));
+  }
+  return res;
+}
+
+console.log(bar(arr));
 //方法二：flat:Infinity
 // console.log(arr.flat(Infinity));
 // //方法三：递归
-function foo(arr) {
-  const res = [];
+// function foo(arr) {
+//   const res = [];
 
-  arr.forEach((i) => {
-    if (Array.isArray(i)) {
-      res.push(...foo(i));
-    } else {
-      res.push(i);
-    }
-  });
-  return res;
-}
-console.log(foo(arr));
+//   arr.forEach((i) => {
+//     if (Array.isArray(i)) {
+//       res.push(...foo(i));
+//     } else {
+//       res.push(i);
+//     }
+//   });
+//   return res;
+// }
+// console.log(foo(arr));
